86812c72bdac1e9317bf8d52f197c3a6cd71967f
Bogdikon <64772515+bogdikon@users.noreply.github.com>
2020-12-03 15:07:30 +0400

    Main file
    Update, added § memo.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+print("You can use § codes!")
+print("§1 Blue\n§2 Light Blue\n§3 Green\n§4 Red\n§5 Pink\n§6 Gold")
 text = input("Text: ")
 print ("@a for all\n @s for you\n @e for all entity\n @r for random player")
 selector = input("Selector: ")
