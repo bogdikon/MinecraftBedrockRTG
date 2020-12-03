5f2c92b36a2767cd9814846d8bda490b1a0ef3bb
Bogdikon <64772515+bogdikon@users.noreply.github.com>
2020-12-03 15:09:51 +0400

    Перевод
    Russian translate

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import time
-print("You can use § codes!")
-print("§1 Blue\n§2 Light Blue\n§3 Green\n§4 Red\n§5 Pink\n§6 Gold")
-text = input("Text: ")
-print ("@a for all\n @s for you\n @e for all entity\n @r for random player")
-selector = input("Selector: ")
+print("Можно использовать цветовые коды!")
+print("§1 Синий\n§2 Голубой\n§3 Зеленый\n§4 Красный\n§5 Розовый\n§6 Золотой")
+text = input("Текст: ")
+print ("@a Для всех\n @s Для вас\n @e Для всех сущностей\n @r Для случайного игрока")
+selector = input("Селектор: ")
 print('/tellraw ' + selector + ' {"rawtext":[{"text":"' + text + '"}]}')
 while True:
     time.sleep(1)
