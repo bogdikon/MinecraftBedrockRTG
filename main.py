982ca8a465ef2d40d73f47a6fefc76c9f2ff7fce
Bogdikon <64772515+bogdikon@users.noreply.github.com>
2020-12-20 20:29:38 +0400

    Update
    Now §r is automated to avoid issues!

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 print("You can use § codes!")
 print("§1 Blue\n§2 Light Blue\n§3 Green\n§4 Red\n§5 Pink\n§6 Gold")
+print("§r now is automated to avoid issues!")
 text = input("Text: ")
 print ("@a for all\n @s for you\n @e for all entity\n @r for random player")
 selector = input("Selector: ")
-print('/tellraw ' + selector + ' {"rawtext":[{"text":"' + text + '"}]}')
+print('/tellraw ' + selector + ' {"rawtext":[{"text":"' + text + '§r"}]}')
 while True:
     time.sleep(1)
